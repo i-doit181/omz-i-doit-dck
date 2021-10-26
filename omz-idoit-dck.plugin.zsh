22b15dbeb0487a52bbae288f3d034730d81d20d1
i-doit181 <salvatore.milite181@gmail.com>
2021-10-26 22:40:26 +0200

    add other commands

--- omz-idoit-dck.plugin.zsh
+++ omz-idoit-dck.plugin.zsh
@@ -1,8 +1,10 @@
 #easy work
 ##open yml,yaml,json,properties files directly with vim
 alias -s {yml,yaml,json,properties}=vim
+
 ##pipeline with grep
 alias -g G=' | grep -i'
+
 #docker
 function docker_login() {
   local proxy=${1:-"index.docker.io"}
@@ -14,11 +16,20 @@ function docker_login() {
   fi
 }
 
+#todo: use docker credential storage instead of volatile credentials
 alias dck_lgn='docker_login $1 $2'
 
 #terraform
 alias trf='docker run -it -w="/.terraform" -v $(pwd)/:/.terraform/ hashicorp/terraform:1.0.7'
 
+#gradle
+alias gradle='docker run -it -w="/project" -v $(pwd)/:/project gradle:7.2-jdk17 gradle'
+
+#nodejs
+alias npm='docker run -it -v $(pwd)/:/project/ -w /project node:slim npm $2'
+
+#openapi
+alias openapi-generator-cli='docker run -it -v $(pwd)/:/project/ -w /project openapitools/openapi-generator-cli:latest-release'
+
 #devops
 alias devops='docker run -it -v $(pwd)/:/project/ salvax86/tools:2.0.0 /bin/bash'
-
